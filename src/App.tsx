40eb1dab79483659f370f869df571e385eba36b2
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-05-02 23:36:45 +0200

    authentication context and changer method

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './App.css';
 import AppHeader from './components/AppHeader/AppHeader';
@@ -6,15 +6,51 @@ import LoginPage from './components/LoginPage';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Startpage';
 
-function App() {
+export type AuthenticationContextType = {
+    sessionId: string;
+    username: string;
+    loggedIn: boolean;
+    login: (s: string, u: string) => void
+}
+
+let contextValue: AuthenticationContextType = {
+    sessionId: "",
+    username: "",
+    loggedIn: false,
+    login: (s: string, u: string) => {}
+}
+
+export const AuthenticationContext = React.createContext(contextValue)
+
+function App(this: any) {
+
+    let [authState, setAuthState] = useState(contextValue)
+
+    useEffect(() => {
+        setAuthState(prevState => {
+            return {
+                ...prevState,
+                login: (s: string, u: string) => {
+                    setAuthState((prev => {
+                        return {...prev,
+                            loggedIn: true,
+                            username: u,
+                            sessionId: s
+                        }
+                    }))
+                }
+            }
+        })
+    }, []);
+
     return (
-      <>
+      <AuthenticationContext.Provider value={authState}>
         <AppHeader/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<LoginPage/>}/>
         </Routes>
-      </>
+      </AuthenticationContext.Provider>
     );
 }
 
--- src/components/AccountIcon/AccountIcon.tsx
+++ src/components/AccountIcon/AccountIcon.tsx
@@ -1,16 +1,19 @@
-import "./AccountIcon.css"
+import React, {useContext} from 'react';
+import "./AccountIcon.css";
 import {Button, Popover} from "@blueprintjs/core";
 import AccountDropdown from "./AccountDropdown";
 import {Link} from "react-router-dom";
+import {AuthenticationContext} from "../../App";
 
 function AccountIcon() {
+    const authenticationContext = useContext(AuthenticationContext)
 
     //TODO global state for loggedin with username, sessionid...
-    let loggedIn = false;
+    let loggedIn = authenticationContext.loggedIn;
 
     return (
         <div className={"accountIcon"}>
-            {!loggedIn && <Button large={true}> <Link to={"/login"} >Login</Link> </Button>}
+            {!loggedIn && <Link to={"/login"}> <Button large={true}> Login </Button> </Link>}
             {loggedIn &&
                 <Popover content={<AccountDropdown/>}>
                     <Button large={true}>
