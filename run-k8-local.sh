bdccd72ae2938e176d370de8fd1d7f1150b73a1a
Smightym8 <michaelspiegel@live.at>
2022-06-04 20:29:59 +0200

    Changed kubernetes to start it local

--- run-k8-local.sh
+++ run-k8-local.sh
@@ -1,11 +1,11 @@
 eval $(minikube docker-env)
 
-kubectl delete -f ./kubernetes/web-frontend-deployment.yml
+kubectl delete -f ./kubernetes/local/web-frontend-deployment.yml
 
 npm run build
 
 docker build -f Dockerfile-kubernetes -t team-f-web-shop-tomify .
 
-kubectl apply -f ./kubernetes/web-frontend-deployment.yml
+kubectl apply -f ./kubernetes/local/web-frontend-deployment.yml
 
 minikube service web-shop-tomify --url
