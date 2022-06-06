8fd5c147381abd9b8c7d7cf44681389ce44c3a2c
Smightym8 <michaelspiegel@live.at>
2022-06-06 13:29:51 +0200

    Removed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,1 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import {MemoryRouter} from "react-router-dom";
-
-test('renders the tomify logo', () => {
-  render(<MemoryRouter>
-    <App/>
-  </MemoryRouter>);
-  const logo = screen.getByAltText("tomify-logo");
-  expect(logo).toBeInTheDocument();
-});
